d48984bb2f1973f9da720ad0d38a23fdb704d0b7
PennyS8 <silliman25@up.edu>
2024-04-20 17:01:42 -0700

    Some edits and corrections

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -15,7 +15,7 @@ def linear_regression(data):
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
 
-    # Split data into training and testing sets
+    # Split data into 20% training and 80% testing sets
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
     # Train Linear Regression Model
--- src/main.py
+++ src/main.py
@@ -1,33 +1,38 @@
-import pandas as pd
+import sys
 import os
+import pandas as pd
 import analysis as ana
 import linear_regression as linr
 import machine_learning as mach
 
-parnet_dir = os.path.dirname(os.path.dirname(__file__))
-csv_file_path = os.path.join(parnet_dir, 'resources', 'Parks_Tree_Inventory.csv')
+def main():
+    parnet_dir = os.path.dirname(os.path.dirname(__file__))
+    csv_file_path = os.path.join(parnet_dir, 'resources', 'Parks_Tree_Inventory.csv')
 
-# If file doesn't exist, then run error and exit program
-try:
-    # Read the CSV file into a DataFrame
-    tree_inv = pd.read_csv(csv_file_path)
-except:
-    SystemExit('Error: CSV file not found at', csv_file_path)
+    # If file doesn't exist, then run error and exit program
+    try:
+        # Read the CSV file into a DataFrame
+        tree_inv = pd.read_csv(csv_file_path)
+    except:
+        SystemExit('Error: CSV file not found at', csv_file_path)
 
-variables = ['DBH', 'TreeHeight', 'CrownWidthNS', 'Pollution_Removal_oz', 'Stormwater_ft']
-label = 'Total_Annual_Benefits'
+    variables = ['DBH', 'TreeHeight', 'CrownWidthNS', 'Pollution_Removal_oz', 'Stormwater_ft']
+    label = 'Total_Annual_Benefits'
 
-tree_data = tree_inv.dropna(subset = variables+[label])
+    tree_data = tree_inv.dropna(subset = variables+[label])
 
-data = {}
-for var in variables+[label]:
-    data[var] = [float(i) for i in tree_data[var]]
+    data = {}
+    for var in variables+[label]:
+        data[var] = [float(i) for i in tree_data[var]]
 
-# Data analysis
-ana.analyze_data(data)
+    # Data analysis
+    ana.analyze_data(data)
 
-# Linear regression
-linr.linear_regression(data)
+    # Linear regression
+    linr.linear_regression(data)
 
-# Machine learning
-mach.machine_learning_plot(data)
+    # Machine learning
+    mach.machine_learning_plot(data)
+
+if __name__ == '__main__':
+    sys.exit(main())
